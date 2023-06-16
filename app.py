a3a9da5f09e964dbb78c97a5952c20108fc729d0
TomoyaOgawa <5stream418@gmail.com>
2023-06-16 23:02:33 +0900

    import flask-restx

--- app.py
+++ app.py
@@ -1,16 +1,37 @@
 from flask import Flask
+from flask_restx import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/', methods=['GET', 'POST'])
+
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+    
+@api.route('/<name>')
+class HelloWorld(Resource):
+    def get(self, name):
+        return {'hello': name}
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+"""
+@api.route('/', methods=['GET', 'POST'])
 def lambda_handler(event=None, context=None):
     
     return 'hello!'
 
-@app.route('/<name>', methods=['GET', 'POST'])
+@api.route('/<name>', methods=['GET', 'POST'])
 def hello_name(name):
     
     return 'hello ' + name
 
 if __name__ == '__main__':
     app.run()
+
+"""
