4ecc08eb035879d62446a3ce5de8a3a1b47b48c3
TomoyaOgawa <5stream418@gmail.com>
2023-06-19 17:20:28 +0900

    updated

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from flask import Flask,Blueprint
-from flask_restx import Api, Resource, Namespace
+from flask import Flask,Blueprint, request
+from flask_restx import Api, Resource, Namespace, fields
+from routers.image import post_image
+from werkzeug.datastructures import FileStorage
 
 
 app = Flask(__name__)
-api = Api(app, version='1.0', title='API', description='A simple API')
 
-@api.route('/image')
+
+api = Api(app, version='1.0', title='API', description='A simple API',default='practice',default_label='practice')
+
+upload_parser = api.parser()
+upload_parser.add_argument('file', location='files',
+                           type=FileStorage, required=True)
+
+
+resource_fields = api.model("Json Body", {
+    "key1": fields.String,
+    "key2": fields.Integer,
+    "key3": fields.Boolean,
+})
+@api.route('/image/<id>')
+@api.expect(upload_parser)
 class ImageConversion(Resource):
-    def post(self):
-        return {'image': 'image', 'message': 'success'}
+    def post(self,id):
+        args = upload_parser.parse_args()
+        uploaded_file = args['file']  # This is FileStorage instance
+        return {'image': 'image', 'message': 'success', 'id': id, 'file': uploaded_file.filename}
+    
+    
     
 @api.route('/hello')
 class HelloWorld(Resource):
@@ -18,8 +37,12 @@ class HelloWorld(Resource):
     
 @api.route('/name/<name>')
 class HelloName(Resource):
-    def get(self, name):
-        return {'hello': name}
+    @api.doc(body=resource_fields)
+    def post(self, name):
+        body = request.json
+        return {
+            'hello': name,
+            'body': body,}
 
 if __name__ == '__main__':
     app.run(debug=True)
