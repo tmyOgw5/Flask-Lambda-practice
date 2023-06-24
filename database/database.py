40f0332ed72c84b0b88a0fce3977bf3e8c324e0a
TomoyaOgawa <5stream418@gmail.com>
2023-06-25 00:06:54 +0900

    initial commit

--- database/database.py
+++ database/database.py
@@ -1,9 +1,18 @@
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-db = SQLAlchemy()
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog_api.db'
 
-class UserPost(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    title = db.Column(db.String, nullable=False)
-    content = db.Column(db.String, nullable=False)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
