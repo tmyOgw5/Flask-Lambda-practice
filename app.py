1b2deffa34274bb28c61f71f8b34ba0064bd9f28
TomoyaOgawa <5stream418@gmail.com>
2023-06-25 13:04:36 +0900

    add

--- app.py
+++ app.py
@@ -2,37 +2,14 @@ from flask import Flask, Blueprint
 from flask_restx import Api
 from routers import get, post
 from flask import Flask
-#from flask_sqlalchemy import SQLAlchemy
-#from database.database import db, UserPost
 from database import models
 from database.database import engine
+from database.database import get_db
 
 
 
 
 app = Flask(__name__)
-"""
-# configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///api.db"
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO']=True
-# initialize the app with the extension
-db.init_app(app)
-
-
-
-with app.app_context():
-    db.create_all()
-
-"""
-
-
-
-
-
-
-
 
 # Blueprintの作成
 api_bp = Blueprint('api', __name__)
@@ -47,11 +24,17 @@ api.models[get.resource_fields.name] = get.resource_fields
 # postモジュールでJson Bodyモデルを登録
 api.models[post.resource_fields.name] = post.resource_fields
 
+api.models[post.post_base_model.name] = post.post_base_model
+api.models[post.post_display_model.name] = post.post_display_model
+
 app.register_blueprint(api_bp, url_prefix='/api/')
 
-
-
 models.Base.metadata.create_all(engine)
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db = get_db()
+    db.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
