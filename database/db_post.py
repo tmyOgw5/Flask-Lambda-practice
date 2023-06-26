436ed17d104c14dd79fb7ab59f27c45f0e0ac8f4
TomoyaOgawa <5stream418@gmail.com>
2023-06-26 16:58:23 +0900

    add database functions

--- database/db_post.py
+++ database/db_post.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm.session import Session
 from database.models import DbPost
 import datetime
+from flask_restx import abort
 
 def create_post(db: Session, request: dict):
     new_post = DbPost(
@@ -14,5 +15,23 @@ def create_post(db: Session, request: dict):
     db.commit()
     db.refresh(new_post)
     return new_post
+
+
+def get_all_posts(db: Session):
+    return db.query(DbPost).all()
+
+def get_post_by_id(db: Session, id: int):
+    post = db.query(DbPost).filter(DbPost.id == id).first()
+    if not post:
+        raise abort(404, 'Post not found')
+    return post
+
+def delete_post(db: Session, id: int):
+    post = db.query(DbPost).filter(DbPost.id == id).first()
+    if not post:
+        raise abort(404, 'Post not found')
+    db.delete(post)
+    db.commit()
+    return post
 
 
--- routers/post.py
+++ routers/post.py
@@ -30,16 +30,49 @@ post_base_model = api.model('PostBase', {
 })
 
 post_display_model = api.model('PostDisplay', {
-    'id': fields.Integer(readOnly=True),
     'image_url': fields.String(required=True),
     'title': fields.String(required=True),
-    'content': fields.String(required=True),
     'creator': fields.String(required=True),
-    'timestamp': fields.DateTime(dt_format='rfc822'),
+    'timestamp': fields.DateTime(required=True),
+    
 })
 
 # Namespaceの作成
 ns = Namespace('post', description='Post operations')
+
+
+
+@ns.route('/')
+class Post(Resource):
+    @api.expect(post_base_model)
+    @api.marshal_with(post_display_model)
+    def post(self):
+        db = get_db()  # Replace this with your actual db session getter
+        data = request.json
+        post = db_post.create_post(db, data)  # Replace this with your actual create method
+        return post
+
+@ns.route('/all')
+class GetALlPosts(Resource):
+    @api.marshal_with(post_display_model)
+    def get(self):
+        db = get_db()
+        posts = db_post.get_all_posts(db)
+        return posts
+
+@ns.route('/user/<int:id>')
+class GetPostsById(Resource):
+    @api.marshal_with(post_display_model)
+    def get(self, id):
+        db = get_db()
+        posts = db_post.get_post_by_id(db, id)
+        return posts
+    
+    def delete(self, id):
+        db = get_db()
+        posts = db_post.delete_post(db, id)
+        return "deleted"
+
 
 @ns.route('/file/')
 @ns.expect(upload_parser)
@@ -51,17 +84,6 @@ class FileUpload(Resource):
 
         return {'image': 'image', 'message': 'success', 'file': uploaded_file.filename}
     
-
-@ns.route('/userpost/')
-class UserPost(Resource):
-    @api.expect(post_base_model)
-    @api.marshal_with(post_display_model)
-    def post(self):
-        db = get_db()  # Replace this with your actual db session getter
-        data = request.json
-        post = db_post.create_post(db, data)  # Replace this with your actual create method
-        return post
-
 # NamespaceをBlueprintに追加
 api.add_namespace(ns)
 
